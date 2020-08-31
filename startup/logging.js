e77b5c3e3fb1654252c4a1ce858625782d9052e0
idtr <ivanlozmar@gmail.com>
2020-08-30 18:46:01 -0700

    .

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,6 @@
 const winston = require('winston');
 require('express-async-errors');
+// require('winston-mongodb');
 
 module.exports = function () {
   winston.handleExceptions(
@@ -12,4 +13,8 @@ module.exports = function () {
   });
 
   winston.add(winston.transports.File, { filename: 'logfile.log' });
+  // winston.add(winston.transports.MongoDB, {
+  //   db: 'mongodb://localhost/rates',
+  //   level: 'info'
+  // });
 };
